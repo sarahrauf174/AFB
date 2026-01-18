b56233fe942b209daeb69b324e5238986c694b15
linaskog <skoglina7@gmail.com>
2026-01-18 22:20:25 +0100

    uppdaterad med kommentarer
    Ändrat för att göra koden mera läsbar

--- cellcountwindow.py
+++ cellcountwindow.py
@@ -14,179 +14,155 @@ class CellCountWindow(QMainWindow):
     def __init__(self, img):
         super().__init__()
 
-        self.setWindowTitle("Cell Counting Window")
         self.img = img
+        self.setWindowTitle("Cell Counting Window")
 
+        # --- Layout structure ---
         main_container = QWidget()
         main_layout = QHBoxLayout(main_container)
-
-        # =========================
         # Control Panel
-        # =========================
         left_container = QWidget()
         left_layout = QVBoxLayout(left_container)
         left_container.setFixedSize(400, 200)
-
-        right_container = QWidget()
-        right_layout = QVBoxLayout(right_container)
-        right_container.setFixedHeight(420)
-
+        # 1st and 2nd row: peak_thresh_frac information and slider
         container_1 = QWidget()
         layout_1 = QHBoxLayout(container_1)
         container_2 = QWidget()
         layout_2 = QHBoxLayout(container_2)
+        # 3rd and 4th row: min_area information and slider
         container_3 = QWidget()
         layout_3 = QHBoxLayout(container_3)
         container_4 = QWidget()
         layout_4 = QHBoxLayout(container_4)
-        container_5 = QWidget()
-        layout_5 = QHBoxLayout(container_5)
+        # Results Display
+        right_container = QWidget()
+        right_layout = QVBoxLayout(right_container)
+        right_container.setMaximumHeight(420)
 
-        # =========================
-        # Peak Thresh Frac labels and sliders
-        # =========================
+
+
+        # --- Peak Thresh Frac controls ---
         ptf_min = QLabel("0.25")
         ptf_max = QLabel("0.5")
         self.ptf_slider = QSlider(Qt.Horizontal)
         self.ptf_slider.setRange(25, 50)
         self.ptf_slider.setValue(35)
+        self.ptf_slider.sliderReleased.connect(self.ptf_changed)
         self.current_ptf = self.ptf_slider.value() / 100
-
-
-        layout_2.addWidget(ptf_min)
-        layout_2.addWidget(self.ptf_slider)
-        layout_2.addWidget(ptf_max)
-
         ptf_label = QLabel("peak_thresh_frac:")
-        ptf_info = InfoButton("Sensitivity of nucleus\n"
-                              "Lower values detect \n" 
+        ptf_info = InfoButton("Sensitivity of cell detection\n"
+                              "Lower values detect \n"
                               "more cells (may over-split)\n"
                               "Higher values detect \n"
-                              "fewer cells (may merge\n" \
+                              "fewer cells (may merge\n" 
                               "cells)")
         self.ptf_value = QLabel(f"{self.current_ptf}")
 
-        layout_1.addWidget(ptf_info)
-        layout_1.addWidget(ptf_label)
-        layout_1.addWidget(self.ptf_value)
-
-        self.ptf_slider.sliderReleased.connect(self.ptf_changed)
-
-        # =========================
-        # Min area labels and sliders
-        # =========================
-
+        # --- Min area controls ---
         min_min = QLabel("20px")
         min_max = QLabel("200px")
         self.min_slider = QSlider(Qt.Horizontal)
         self.min_slider.setRange(20, 200)
         self.min_slider.setValue(40)
-        self.current_min = self.min_slider.value()
-
-        layout_4.addWidget(min_min)
-        layout_4.addWidget(self.min_slider)
-        layout_4.addWidget(min_max)
-
-        min_info = InfoButton("Minimum size counted\n" \
-        "as cell during area \n" \
-        "filtration step.")
-        min_area_label = QLabel("min_area:")
-        self.min_area_value = QLabel(f"{self.current_min}px" )
-
-        layout_3.addWidget(min_info)
-        layout_3.addWidget(min_area_label)
-        layout_3.addWidget(self.min_area_value)
-
         self.min_slider.sliderReleased.connect(self.min_area_changed)
+        self.current_min = self.min_slider.value()
+        min_info = InfoButton("Minimum size counted\n" 
+                              "as cell during area \n" 
+                              "filtration step.")
+        min_area_label = QLabel("min_area:")
+        self.min_area_value = QLabel(f"{self.current_min}px")
 
-        # =========================
-        # Cancel button 
-        # =========================
-
+        # --- Cancel controls ---
         cancel_button = QPushButton("Cancel")
         cancel_button.clicked.connect(self.cancel)
         cancel_button.setFixedSize(100, 25)
 
+        # --- Initial cellcount ---
+        self.analysis = CellCount(img=self.img,
+                                  peak_thresh_frac=self.current_ptf,
+                                  min_area=self.current_min)
 
+        # --- Result display ---
+        self.cell_count_label = QLabel(f"Cell Count: {self.analysis.run()[0]}")
+        self.image = QLabel()
+        h, w = img.shape[:2]
+        display_w = min(400, w)
+        display_h = min(400, h)
+        self.image.setFixedSize(display_w, display_h)
+
+
+        # Widgets added left to right
+        layout_1.addWidget(ptf_info)
+        layout_1.addWidget(ptf_label)
+        layout_1.addWidget(self.ptf_value)
+        layout_2.addWidget(ptf_min)
+        layout_2.addWidget(self.ptf_slider)
+        layout_2.addWidget(ptf_max)
+        layout_3.addWidget(min_info)
+        layout_3.addWidget(min_area_label)
+        layout_3.addWidget(self.min_area_value)
+        layout_4.addWidget(min_min)
+        layout_4.addWidget(self.min_slider)
+        layout_4.addWidget(min_max)
+        # Widgets added top to bottom
         left_layout.addWidget(container_1)
         left_layout.addWidget(container_2)
         left_layout.addWidget(container_3)
         left_layout.addWidget(container_4)
         left_layout.addWidget(cancel_button)
 
-
-
-        main_layout.addWidget(left_container)
-
-
-        # =========================
-        # Intitial count using preset peak_thresh_frac and min_area values
-        # =========================
-        self.analysis = CellCount(img = self.img,
-                                     peak_thresh_frac = self.current_ptf,
-                                     min_area = self.current_min)
-        
-
-        # =========================
-        # Display current cell count and preview
-        # =========================
-        self.cell_count_label = QLabel(f"Cell Count: {self.analysis.run()[0]}")
-        self.image = QLabel()
-        self.image.setFixedSize(400, 400)
-
+        # Widgets added top to bottom
         right_layout.addWidget(self.cell_count_label)
         right_layout.addWidget(self.image)
+        # Widgets added left to right
+        main_layout.addWidget(left_container)
         main_layout.addWidget(right_container)
 
-        self.update_preview()
-        
         self.setCentralWidget(main_container)
+
+        # --- Displays results ---
+        self.update_preview()
 
     def show_image(self, img):
         """
-        input: img (loaded with imread (tiff or cv))
+        input: img
         creates QImage from img and QPixmap from QImage
         Pixmap displayed as preview image in CellCountWindow
         """
-        if len(img.shape) == 2:
+        if img.ndim == 2:
             img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
-        img_copy = img.copy()
-        rgb_img = cv.cvtColor(img_copy, cv.COLOR_BGR2RGB)
-        h, w, ch = rgb_img.shape
-        bytesPerLine = 3 * w
-        qImg = QImage(rgb_img.data, w, h, bytesPerLine, QImage.Format_RGB888)
-        pixmap = QPixmap.fromImage(qImg).scaled(self.image.width(), self.image.height(), Qt.KeepAspectRatio,
+
+        h, w = img.shape[:2]
+        qimg = QImage(img.data, w, h, img.strides[0],QImage.Format_BGR888)
+        pixmap = QPixmap.fromImage(qimg)
+        pixmap = pixmap.scaled(self.image.width(), self.image.height(), Qt.KeepAspectRatio,
                                                 Qt.SmoothTransformation)
         self.image.setPixmap(pixmap)
+
 
     def update_preview(self):
         """
         Runs CellCount with current peak_thresh_frac and min_area values
-        Displays updated preview with show_image
-        Displays updated cell count in CellCountWindow
+        Displays updated preview and updated cell count in CellCountWindow
         """
         self.analysis.peak_thresh_frac = self.current_ptf
         self.analysis.min_area = self.current_min
-        count, preview,  = self.analysis.run()[:2]
+        count, preview, = self.analysis.run()[:2]
         self.show_image(preview)
         self.cell_count_label.setText(f"Cell Count: {count}")
 
     def ptf_changed(self):
         """
-        Calculates current ptf value from slider position
-        Updates ptf value displayes in CellCountWindow
-        Updates preview and cell count with update_preview
+        Update slider value and update preview
         """
         pos = self.ptf_slider.value()
-        self.current_ptf = pos/100
+        self.current_ptf = pos / 100
         self.ptf_value.setText(f"{self.current_ptf}")
         self.update_preview()
 
     def min_area_changed(self):
         """
-        Updates current min_area value from slider position
-        Updates min_area value displayes in CellCountWindow
-        Updates preview and cell count with update_preview
+        Update slider value and update preview
         """
         pos = self.min_slider.value()
         self.current_min = pos
--- curvedrawingwidget.py
+++ curvedrawingwidget.py
@@ -1,5 +1,3 @@
-import cv2 as cv
-import numpy as np
 from PySide6.QtCore import Qt, QPoint
 from PySide6.QtGui import QImage, QPixmap, QPainter, QPen
 from PySide6.QtWidgets import QWidget
@@ -8,53 +6,65 @@ from PySide6.QtWidgets import QWidget
 class CurveDrawingWidget(QWidget):
     def __init__(self, img):
         super().__init__()
-
         self.img = img
-        self.orig_h, self.orig_w = img.shape[:2]
 
-        # Sets maximum display size (preserves aspect ratio)
-        self.display_w = min(400, self.orig_w)
-        self.display_h = min(400, self.orig_h)
-        self.pixmap = self.get_scaled_pixmap()
-        self.setFixedSize(self.pixmap.width(), self.pixmap.height())
-
-        # =========================
-        # Storage for curves
-        # =========================
+        # --- Storage for curves ---
         self.start_curve = []
         self.stop_curve = []
         self.mode = "start"
 
+        # --- Setting display image size and generating pixmap ---
+        self.orig_h, self.orig_w = img.shape[:2]
+        self.display_w = min(400, self.orig_w)
+        self.display_h = min(400, self.orig_h)
 
+        self.pixmap = self.get_scaled_pixmap()
+        self.setFixedSize(self.pixmap.width(), self.pixmap.height())
 
     def get_scaled_pixmap(self):
         """
         Creates pixmap from input image
-        Scales pixmap down
+        Scales pixmap size to widget size
         """
-
         img = self.img
-        h, w = img.shape[:2]
-
-        qimg = QImage(img.data, w, h, img.strides[0], QImage.Format_RGB888)
-
-        img = np.ascontiguousarray(img)  
-        h, w = img.shape[:2]
+        if img.ndim == 2:
+            qimg = QImage(img.data, self.orig_w, self.orig_h, img.strides[0], QImage.Format_Grayscale8)
+        elif img.ndim == 3 and img.shape[2] == 3:
+            qimg = QImage(img.data, self.orig_w, self.orig_h, img.strides[0], QImage.Format_RGB888)
+        else:
+            raise ValueError(f"Unsupported image shape: {img.shape}")
 
         pixmap = QPixmap.fromImage(qimg)
         pixmap = pixmap.scaled(self.display_w, self.display_h, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+
         return pixmap
+
+    def mousePressEvent(self, event):
+        """
+        Adds clicked point to the currently selected curve, updates line on display
+        """
+        if event.button() == Qt.LeftButton:
+            #Coordinates scaled from display to original image coordinates
+            scale_x = self.orig_w / self.pixmap.width()
+            scale_y = self.orig_h / self.pixmap.height()
+            x = int(event.position().x() * scale_x)
+            y = int(event.position().y() * scale_y)
+
+            #Coordinate point added to curve list
+            if self.mode == "start":
+                self.start_curve.append((x, y))
+            elif self.mode == "stop":
+                self.stop_curve.append((x, y))
+
+            self.update()
 
     def paintEvent(self, event):
         """
-        Paint curves on display image. 
-        Red color for start curve,
-        blue color for stop curve. Draws line between points in start/stop curves.
+        Draw start (red) and stop (blue) curves on pixmap (display)
         """
         painter = QPainter(self)
         if self.pixmap:
             painter.drawPixmap(0, 0, self.pixmap)
-
         start_pen = QPen(Qt.red, 2)
         stop_pen = QPen(Qt.blue, 2)
 
@@ -64,7 +74,6 @@ class CurveDrawingWidget(QWidget):
                 p1 = self.to_display_coords(self.start_curve[i-1])
                 p2 = self.to_display_coords(self.start_curve[i])
                 painter.drawLine(p1, p2)
-
         if len(self.stop_curve) > 1:
             painter.setPen(stop_pen)
             for i in range(1, len(self.stop_curve)):
@@ -74,27 +83,15 @@ class CurveDrawingWidget(QWidget):
 
     def to_display_coords(self, point):
         """
-        Scaled coordinates of points
+        Convert original image coordinates to display coordinates
         """
         x, y = point
         scale_x = self.pixmap.width() / self.orig_w
         scale_y = self.pixmap.height() / self.orig_h
         return QPoint(int(x * scale_x), int(y * scale_y))
 
-    def mousePressEvent(self, event):
-        """
-        Writes coordinates from left click on image to curves
-        """
-        if event.button() == Qt.LeftButton:
 
-            scale_x = self.orig_w / self.pixmap.width()
-            scale_y = self.orig_h / self.pixmap.height()
-            x = int(event.position().x() * scale_x)
-            y = int(event.position().y() * scale_y)
 
-            if self.mode == "start":
-                self.start_curve.append((x, y))
-            elif self.mode == "stop":
-                self.stop_curve.append((x, y))
 
-            self.update()
+
+
